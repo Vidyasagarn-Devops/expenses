609299db9cb9b82b493fc47039e758bc1d00de72
sagar nellipudi <sagar.nf@gmail.com>
2024-05-14 22:08:53 +0530

    funxtions

---
+++ backend.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter DB Password:"
+read -s mysql_root_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..... $R Failure $N"
+        exit 1
+    else
+        echo -e "$2.... $G Success $N"
+    fi
+
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default version of NodeJs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJs Version of 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJs"
+
+useradd expense &>>$LOG_FILE
+VALIDATE $? "User Addition"
+
