509e4f8f7f0fd18347c54650653ebcaaa4f22526
sagar nellipudi <sagar.nf@gmail.com>
2024-05-14 21:32:39 +0530

    funxtions

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "Please enter DB Password:"
+read -s mysql_root_password
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -41,11 +44,11 @@ VALIDATE $? "Starting MySql Server"
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
 #VALIDATE $? "Settingup root Password"
 
-mysql -h db.nelipudidevops.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$LOG_FILE
+mysql -h db.nelipudidevops.online -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$LOG_FILE
 
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1
+    mysql_secure_installation --set-root-pass ${mysql_root_password}
 else
     echo -e "MySql root password is already setup.... $Y SKIPPING $N"
 fi
