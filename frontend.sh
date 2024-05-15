6f049c1af21c0f492cab02ce0b7786cd7ae2ed9a
sagar nellipudi <sagar.nf@gmail.com>
2024-05-15 11:47:07 +0530

    functions

--- frontend.sh
+++ frontend.sh
@@ -40,15 +40,21 @@ VALIDATE $? "Enabling nginx"
 systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing existing contents"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Frontend Code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting HTML Code"
 
+cp /home/ec2-user/expenses/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE
+VALIDATE $? "copied expense conf"
+
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "restart nginx"
 
 
